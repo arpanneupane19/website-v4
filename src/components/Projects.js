6f7711e3b8989dcb6d3240f17ee185a8f609e430
Arpan Neupane <arpanneupane19@gmail.com>
2021-07-18 15:47:31 -0500

    added bounce cascade animation to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Projects.css'
 import { ChangeTitle } from './Home.js'
+import Bounce from 'react-reveal/Bounce'
 import Bloggy from './images/bloggy.png';
 import SecuroChat from './images/securochat.png';
 import WeatherWizard from './images/weatherwiz.png';
@@ -17,91 +18,92 @@ function Projects() {
         <div className="font-sans antialiased">
             <div className="projects">
                 <div className="flex flex-col justify-center items-center pt-36 pb-24 lg:px-16 px-6">
-                    <p className="projects-header md:text-3xl text-2xl mb-4 font-medium">Projects</p>
-
-                    <div className="flex flex-row flex-wrap justify-center items-center">
-                        <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
-                            <p className="text-xl text-center font-light mb-2">Bloggy</p>
-                            <img src={Bloggy} alt="Bloggy" />
-                            <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">Python</p>
+                    <Bounce bottom cascade>
+                        <p className="projects-header md:text-3xl text-2xl mb-4 font-medium">Projects</p>
+                        <div className="flex flex-row flex-wrap justify-center items-center">
+                            <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
+                                <p className="text-xl text-center font-light mb-2">Bloggy</p>
+                                <img src={Bloggy} alt="Bloggy" />
+                                <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">Python</p>
+                                    </div>
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">Socket.io</p>
+                                    </div>
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">PostgreSQL</p>
+                                    </div>
                                 </div>
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">Socket.io</p>
+                            </div>
+                            <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
+                                <p className="text-xl text-center font-light mb-2">SecuroChat</p>
+                                <img src={SecuroChat} alt="SecuroChat" />
+                                <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">React.js</p>
+                                    </div>
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">Node.js</p>
+                                    </div>
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">Socket.io</p>
+                                    </div>
                                 </div>
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">PostgreSQL</p>
+                            </div>
+                            <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
+                                <p className="text-xl text-center font-light mb-2">Weather Wizard</p>
+                                <img src={WeatherWizard} alt="Weather Wizard" />
+                                <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">React.js</p>
+                                    </div>
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">OpenWeatherMap API</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
+                                <p className="text-xl text-center font-light mb-2">Todos</p>
+                                <img src={Todos} alt="Todos" />
+                                <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">Python</p>
+                                    </div>
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">HTML/CSS</p>
+                                    </div>
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">SQLite</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
+                                <p className="text-xl text-center font-light mb-2">This Website</p>
+                                <img src={Website} alt="Website" />
+                                <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">React.js</p>
+                                    </div>
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">Tailwind CSS</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
+                                <p className="text-xl text-center font-light mb-2">YouTube Clone</p>
+                                <img src={YTClone} alt="YouTube Clone" />
+                                <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">React.js</p>
+                                    </div>
+                                    <div className="tool p-1 rounded-lg m-1">
+                                        <p className="font-light text-center">Firebase</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
-                            <p className="text-xl text-center font-light mb-2">SecuroChat</p>
-                            <img src={SecuroChat} alt="SecuroChat" />
-                            <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">React.js</p>
-                                </div>
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">Node.js</p>
-                                </div>
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">Socket.io</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
-                            <p className="text-xl text-center font-light mb-2">Weather Wizard</p>
-                            <img src={WeatherWizard} alt="Weather Wizard" />
-                            <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">React.js</p>
-                                </div>
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">OpenWeatherMap API</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
-                            <p className="text-xl text-center font-light mb-2">Todos</p>
-                            <img src={Todos} alt="Todos" />
-                            <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">Python</p>
-                                </div>
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">HTML/CSS</p>
-                                </div>
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">SQLite</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
-                            <p className="text-xl text-center font-light mb-2">This Website</p>
-                            <img src={Website} alt="Website" />
-                            <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">React.js</p>
-                                </div>
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">Tailwind CSS</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="project md:py-7 md:px-6 py-5 px-4 flex flex-col mx-3 my-3">
-                            <p className="text-xl text-center font-light mb-2">YouTube Clone</p>
-                            <img src={YTClone} alt="YouTube Clone" />
-                            <div className="tools flex flex-wrap justify-evenly items-center mt-1.5">
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">React.js</p>
-                                </div>
-                                <div className="tool p-1 rounded-lg m-1">
-                                    <p className="font-light text-center">Firebase</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    </Bounce>
                 </div>
             </div>
 
