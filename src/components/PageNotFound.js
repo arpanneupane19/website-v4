fc249e772adb80783778965102b15c004145800f
Arpan Neupane <arpanneupane19@gmail.com>
2021-07-25 13:27:14 -0500

    removed bg color

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -5,7 +5,7 @@ function PageNotFound() {
         <div className="font-sans antialiased bg-white">
             <div className="not-found-page">
                 <div className="flex flex-col justify-center items-center pt-36 pb-24 lg:px-16 px-6">
-                    <div className='bg-green rounded-xl p-6 flex flex-col justify-center items-center text-center'>
+                    <div className='rounded-xl p-6 flex flex-col justify-center items-center text-center'>
                         <p className='not-found-header md:text-3xl text-2xl mb-4'>404 Page Not Found</p>
                         <p className="font-light">Oops! The page you're looking for does not exist.</p>
                     </div>
