a092cd4cf135c29f244d3570b8c23603784aa32e
Arpan Neupane <arpanneupane19@gmail.com>
2021-07-25 12:55:10 -0500

    minor fixes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import NotFoundPage from './components/NotFoundPage'
+import PageNotFound from './components/PageNotFound'
 import ScrollToTop from './components/ScrollToTop'
 
 function App() {
@@ -34,7 +34,7 @@ function App() {
           <Navbar
             brandLink={"link"}
           />
-          <NotFoundPage />
+          <PageNotFound />
         </Route>
       </Switch>
     </Router>
