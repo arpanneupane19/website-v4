0f5a613b9a5727a54eadc4845f5416f913fda832
Arpan Neupane <arpanneupane19@gmail.com>
2021-07-25 12:53:39 -0500

    added not found route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import ScrollToTop from './components/ScrollToTop';
+import NotFoundPage from './components/NotFoundPage'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
@@ -28,6 +29,12 @@ function App() {
             brandLink={"link"}
           />
           <Contact />
+        </Route>
+        <Route path="*">
+          <Navbar
+            brandLink={"link"}
+          />
+          <NotFoundPage />
         </Route>
       </Switch>
     </Router>
