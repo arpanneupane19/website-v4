0eff4702bafd55611c74de50b4f04919782afee0
Arpan Neupane <arpanneupane19@gmail.com>
2021-07-25 13:27:26 -0500

    removed redirect import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Projects from './components/Projects'
