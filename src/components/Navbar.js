7cf95d9ddaf2f1b3aabbaf5a814a4e06222eeb3e
Arpan Neupane <arpanneupane19@gmail.com>
2021-07-25 12:28:11 -0500

    added white bg to navbar and added custom tailwind classes to links and name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom'
 
 function Navbar({ brandLink }) {
     return (
-        <nav className='navbar flex md:flex-row flex-col justify-between items-center md:py-6 py-5 md:px-24 px-8 font-sans antialiased'>
+        <nav className='navbar bg-white flex md:flex-row flex-col justify-between items-center md:py-6 py-5 md:px-24 px-8 font-sans antialiased'>
             <div className="branding md:text-3xl text-2xl">
                 {brandLink === "link" ? (
                     <Link to='/' className="flex">
-                        <p className="first">Arpan</p>
-                        <p className="last">Neupane</p>
+                        <p className="text-first-light">Arpan</p>
+                        <p className="text-last-light">Neupane</p>
                     </Link>
                 ) : (
                     <a href="#home" className="flex">
-                        <p className="first">Arpan</p>
-                        <p className="last">Neupane</p>
+                        <p className="text-first-light">Arpan</p>
+                        <p className="text-last-light">Neupane</p>
                     </a>
                 )}
             </div>
             <ul className='flex flex-center md:mt-0 mt-3 md:text-xl text-base'>
-                <li className='mx-3 nav-link'><a href="/#about">About</a></li>
-                <li className='mx-3 nav-link'><Link to="/projects">Projects</Link></li>
-                <li className='mx-3 nav-link'><Link to="/contact">Contact</Link></li>
+                <li className='mx-3 text-nav-link-light hover:text-nav-link-light-hover'><a href="/#about">About</a></li>
+                <li className='mx-3 text-nav-link-light hover:text-nav-link-light-hover'><Link to="/projects">Projects</Link></li>
+                <li className='mx-3 text-nav-link-light hover:text-nav-link-light-hover'><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
     )
